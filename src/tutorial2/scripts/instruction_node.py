9a937969746231073bf6dc04bc77e2728e17298c
guan <guanerjiage0625@gmail.com>
2020-04-30 20:48:29 +0200

    clean up again

--- src/tutorial2/scripts/instruction_node.py
+++ src/tutorial2/scripts/instruction_node.py
@@ -7,7 +7,9 @@ def talker():
     rospy.init_node('instruction_node', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
-        msg = raw_input('Type a instruction:\n h for home position\n r for repetitive movement\n f for symetric movement\n') 
+        msg = raw_input('Type a instruction:\n h for home position\n \
+            r for repetitive movement\n f for symetric movement\n \
+                n for cancle right hand movement\n') 
         pub.publish(msg)
         rate.sleep()
     
